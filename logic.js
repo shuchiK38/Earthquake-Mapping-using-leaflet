a940f594a2c3ebf782ddccb3bd89bf117a1144de
shuchi <shuchi.khandelwal@gmail.com>
2021-10-02 14:19:27 -0500

    final

--- logic.js
+++ logic.js
@@ -1,11 +1,51 @@
 //Store our API endpoint as queryUrl.
-var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson";
+var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 // Perform a GET request to the query URL/
 d3.json(queryUrl).then(function (data) {
   // Once we get a response, send the data.features object to the createFeatures function.
   createFeatures(data.features);
 });
+function getradius(feature){
+  //console.log(feature.properties.mag);
+  return (feature.properties.mag)
+}
 
+function getcolor(feature){
+  //console.log(feature.geometry.coordinates[2]);
+  let mydepth = feature.geometry.coordinates[2];
+  let mycolor = "#DFFF00";
+  if ( mydepth > 90) {
+    mycolor = "red"
+  }
+
+  else if (mydepth >80){
+    mycolor ="orange"
+  }
+  else if (mydepth >70){
+    mycolor= "blue"
+  }
+  else if (mydepth >60){
+    mycolor ="#CD5C5C"
+  }
+  else if (mydepth >50){
+    mycolor = "brown"
+  }
+  else if (mydepth > 40){
+    mycolor ="teal"
+  }
+  else if (mydepth > 30){
+    mycolor = "#F08080"
+  }
+
+  else if (mydepth>20){
+    mycolor = "#DE3163"
+  }
+
+  else if (mydepth>10){
+    mycolor ="#40E0D0"
+  }
+  return(mycolor)
+}
 function createFeatures(earthquakeData) {
 
   // Define a function that we want to run once for each feature in the features array.
@@ -19,23 +59,29 @@ function createFeatures(earthquakeData) {
   var earthquakes = L.geoJSON(earthquakeData, {
    pointToLayer : function(feature,latlng){
      return new L.CircleMarker(latlng,{
-       radius : 10,
-       color : 	'green',
-      fillOpacity: 0.75,
-      fillColor: 'green'
+       radius : getradius(feature),
+       color : 	getcolor(feature),
+      fillOpacity: 100,
+      
      });
    },
     onEachFeature: onEachFeature
   });
 
 // A function to determine the marker size based on the magnitude
-function markerSize(mag) {
-  return Math.sqrt(mag) * 100;
-}
+//function markerSize(mag) {
+  //return Math.sqrt(mag) * 100;
+//}
 
 //Define arrays to hold the created magnitude and deepth marker.
-Mag = [];
-deepthOfMag = [];
+//var cityMarkers = [];
+//var mag = [];
+
+//Loop through locations and create markers as per depth
+//for (var i= 10; i< mag.length;i++){
+//setting marker raduis for earthquake by passing mag into markersSize function
+
+//}
   // Send our earthquakes layer to the createMap function/
   createMap(earthquakes);
 }
